b84fa1c9cc37726a0343f0da0d52754d0b3d9afa
farukxx11 <farukxx1122@gmail.com>
2025-09-27 14:37:57 +0600

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ export default function Navbar() {
       <h1 className="text-[18px] md:text-2xl font-bold text-gray-600">
         CS-Ticket System
       </h1>
+      {/* Navbar */}
       <div className="hidden md:flex items-center gap-6">
         <a href="">Home</a>
         <a href="">FAQ</a>
